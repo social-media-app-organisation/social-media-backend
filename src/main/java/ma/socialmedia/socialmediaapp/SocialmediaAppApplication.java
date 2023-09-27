a3608556cdb6e9b0c6fddfe51f0c78d65657981d
ChaimaeBougattaya <chaimaebougattaya1@gmail.com>
2023-09-27 15:21:44 +0100

    initial-commit

---
+++ src/main/java/ma/socialmedia/socialmediaapp/SocialmediaAppApplication.java
@@ -0,0 +1,13 @@
+package ma.socialmedia.socialmediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/socialmedia/socialmediaapp/SocialmediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.socialmedia.socialmediaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
