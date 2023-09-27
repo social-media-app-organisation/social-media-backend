f757fcf81321be40f695b6375c795f72cf1e0627
ChaimaeBougattaya <chaimaebougattaya1@gmail.com>
2023-09-27 17:17:06 +0100

    add-dockerfile

--- src/main/java/ma/socialmedia/socialmediaapp/SocialmediaAppApplication.java
+++ src/main/java/ma/socialmedia/socialmediaapp/SocialmediaAppApplication.java
@@ -2,12 +2,21 @@ package ma.socialmedia.socialmediaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SocialmediaAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialmediaAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String get(){
+		return "hello";
 	}
 
 }
